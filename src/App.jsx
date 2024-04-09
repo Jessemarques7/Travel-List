a3c8e86679622b454bb79d34caa0fe6f2af4dc51
Jesse Marques <jessemateus919@gmail.com>
2024-04-09 14:54:18 -0300

    Terceiro commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 3, description: "Charger", quantity: 1, packed: true },
-];
+//Data
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: true },
+//   { id: 3, description: "Charger", quantity: 1, packed: true },
+// ];
+
+//Header
 
 function Logo() {
   return <h1>🌴 Far Away 💼</h1>;
 }
 
-function Form() {
+//Form
+
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
+
   const numbers = Array.from({ length: 20 }, (_, i) => i + 1);
 
   function handleSubmit(e) {
@@ -20,6 +26,8 @@ function Form() {
     if (!description) return;
 
     const newItem = { description, quantity, packed: false, id: Date.now() };
+
+    onAddItems(newItem);
 
     setDescription("");
     setQuantity(1);
@@ -48,17 +56,21 @@ function Form() {
     </form>
   );
 }
-function PackingList() {
+
+//List
+function PackingList({ Items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {Items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
   );
 }
+
+//Item
 
 function Item({ item }) {
   return (
@@ -71,6 +83,8 @@ function Item({ item }) {
   );
 }
 
+//Footer
+
 function Stats() {
   return (
     <footer className="stats">
@@ -79,12 +93,20 @@ function Stats() {
   );
 }
 
+//App
+
 function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList Items={items} />
       <Stats />
     </div>
   );
